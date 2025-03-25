bca1eb1cbb9fee898438cfe172ea78286afa2e08
Lasse Hundt Hansen <lasse.hansen@danfoss.com>
2025-03-25 12:01:00 +0100

    Added generic and non generic repository

---
+++ eShop.Repository/Repositories/Repository/BrandRepository.cs
@@ -0,0 +1,40 @@
+﻿using eShop.Repository.Data;
+using eShop.Repository.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShop.Repository.Repositories.Repository
+{
+    public class BrandRepository
+    {
+        private readonly eShopDbContext _dbContext;
+
+        public BrandRepository(eShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Brand>> GetAllAsync() => await _dbContext.Brands.ToListAsync();
+
+        public async Task<Brand?> GetByIdAsync(int id) => await _dbContext.Brands.FindAsync(id) ?? throw new KeyNotFoundException($"Brand with id {id} not found.");
+
+        public async Task AddAsync(Brand newBrand)
+        {
+            await _dbContext.AddAsync(newBrand);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Brand updatedBrand)
+        {
+            _dbContext.Brands.Update(updatedBrand);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            Brand brand = await GetByIdAsync(id);
+
+            _dbContext.Brands.Remove(brand);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ eShop.Repository/Repositories/Repository/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using eShop.Repository.Data;
+using eShop.Repository.Repositories.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShop.Repository.Repositories.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly eShopDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(eShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+        public async Task<T> GetByIdAsync(int id) => await _dbSet.FindAsync(id) ?? throw new KeyNotFoundException($"Brand with id {id} not found.");
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            T entity = await GetByIdAsync(id);
+
+            _dbSet.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
