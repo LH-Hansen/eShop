adf030d8ecd9727968db3be39cd6646fb85df507
Lasse Hundt Hansen <lasse.hansen@danfoss.com>
2025-03-25 08:55:07 +0100

    Added basic db context.

---
+++ eShop.Repository/Data/eShopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eShop.Repository.Data
+{
+    public class eShopDbContext : DbContext
+    {
+        public eShopDbContext(DbContextOptions<eShopDbContext> options) : base(options) { }
+
+        protected void OnConfiguration(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
