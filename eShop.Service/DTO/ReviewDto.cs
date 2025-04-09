4505ac547acf64a792f3d89150b2157d6357b840
Lasse Hundt Hansen <lasse.hansen@danfoss.com>
2025-04-09 14:13:17 +0200

    Added Review Service, Dto and mapper.

---
+++ eShop.Service/DTO/ReviewDto.cs
@@ -0,0 +1,13 @@
+﻿using eShop.Repository.Entities;
+
+namespace eShop.Service.DTO
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string? Body { get; set; }
+        public int ProductId { get; set; }
+        public required Product Product { get; set; }
+    }
+}
---
+++ eShop.Service/Services/IService/IReviewService.cs
@@ -0,0 +1,14 @@
+﻿using eShop.Service.DTO;
+
+namespace eShop.Service.Services.IService
+{
+    public interface IReviewService
+    {
+        Task AddAsync(ReviewDto reviewDto);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<ReviewDto>> GetAllAsync();
+        Task<ReviewDto> GetByIdAsync(int id);
+        Task<IEnumerable<ReviewDto>> GetByProductIdAsync(int productId);
+        Task UpdateAsync(ReviewDto reviewDto);
+    }
+}
